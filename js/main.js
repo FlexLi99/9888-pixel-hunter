4fd81272a59a6c84bf30f1d366def83aed3e9a2a
Alex <alexfilenko@gmail.com>
2018-04-02 13:21:57 +0300

    #3 review edits

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
-const tempArr = [...document.querySelectorAll(`template`)];
+const templates = [...document.querySelectorAll(`template`)];
 const mainFrame = document.querySelector(`main.central`);
 const frameChange = (number) => {
-  tempArr.forEach((frame, i) => {
-    if (number === i) {
-      mainFrame.innerHTML = ``;
-      mainFrame.appendChild(frame.content.cloneNode(true));
-    }
-  });
+  const frame = templates[number];
+  mainFrame.innerHTML = ``;
+  mainFrame.appendChild(frame.content.cloneNode(true));
 };
-const keyboardSwitch = (e) => {
-  if (e.altKey && e.which === 39 && frameNumber < tempArr.length - 1) {
-    frameNumber += 1;
-    frameChange(frameNumber);
-  } else if (e.altKey && e.which === 37 && frameNumber !== 0) {
-    frameNumber -= 1;
-    frameChange(frameNumber);
+const keyboardHandler = (e) => {
+  const ARROW_RIGHT = 39;
+  const ARROW_LEFT = 37;
+  if (e.altKey && e.which === ARROW_RIGHT && frameNumber < templates.length - 1) {
+    frameChange(++frameNumber);
+  } else if (e.altKey && e.which === ARROW_LEFT && frameNumber !== 0) {
+    frameChange(--frameNumber);
   }
 };
 let frameNumber = 0;
 
 frameChange(frameNumber);
-document.addEventListener(`keydown`, (e) => keyboardSwitch(e));
+document.addEventListener(`keydown`, keyboardHandler);
